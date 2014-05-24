8131f11c7cd246eff61cad57ef3c5a75618966e7
Tim <timothy.colpitts@gmail.com>
2014-05-24 10:19:33 -0500

    added competiton template

--- tutorial.c
+++ tutorial.c
@@ -24,11 +24,79 @@ will need to select what motor it is that you are using.For this example program
                                                                                  //|
 //once you have chosen your motors click apply and ok. Then your code should       |
 //look a little like this ---------------------------------------------------------
-//next we will learn about Task Main.
+//Next you will learn about the VEX competition template
 
+/*  
+    Under here is #pragma are part of the template, defining that you are using Vex, as well as your auto and driver control.  
+*/
+#pragma platform(VEX)
 
-task main ()//this "task main" is the code for telling the cortex what to do when (x) button is pressed.
+//Competition Control and Duration Settings
+#pragma competitionControl(Competition)
+#pragma autonomousDuration(15)
+#pragma userControlDuration(105)
+
+#include "Vex_Competition_Includes.c"   //Main competition background code...do not modify!
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+//                          Pre-Autonomous Functions
+//
+// You may want to perform some actions before the competition starts. Do them in the
+// following function.
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+void pre_auton()
 {
-	while(1==1)//this says while 1==1 which means that the program
+  // Set bStopTasksBetweenModes to false if you want to keep user created tasks running between
+  // Autonomous and Tele-Op modes. You will need to manage all user created tasks if set to false.
+  bStopTasksBetweenModes = true;
+
+	// All activities that occur before the competition starts
+	// Example: clearing encoders, setting servo positions, ...
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+//                                 Autonomous Task
+//
+// This task is used to control your robot during the autonomous phase of a VEX Competition.
+// You must modify the code to add your own robot specific commands here.
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+task autonomous()
+{
+  // .....................................................................................
+  // Insert user code here.
+  // .....................................................................................
+
+	AutonomousCodePlaceholderForTesting();  // Remove this function call once you have "real" code.
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+//                                 User Control Task
+//
+// This task is used to control your robot during the user control phase of a VEX Competition.
+// You must modify the code to add your own robot specific commands here.
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+task usercontrol()
+{
+	// User control code here, inside the loop
+
+	while (true)
 	{
-motor
+	  // This is the main execution loop for the user control program. Each time through the loop
+	  // your program should update motor + servo values based on feedback from the joysticks.
+
+	  // .....................................................................................
+	  // Insert user code here. This is where you use the joystick values to update your motors, etc.
+	  // .....................................................................................
+
+	  UserControlCodePlaceholderForTesting(); // Remove this function call once you have "real" code.
+	}
+}
